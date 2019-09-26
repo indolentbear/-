ada96d569861fbee2c85857e86f4f63f0028d927
indolentbear <uniquexxy2634@163.com>
2019-09-27 06:38:43 +0800

    Update instertionsort.cpp

--- sort/instertionsort.cpp
+++ sort/instertionsort.cpp
@@ -1,28 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int arr[1000],data[1000]={0};
 int n;
+void mergesort(int low, int high);
+void mergetoget(int low, int high, int mid);
 
 int main()
 {
     cin>>n;
-    int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
-
-    for(int i=1;i<n;i++)
-    {
-        int key = arr[i];
-        int j = i-1;
-        while(j>=0 && arr[j]>key)
-        {
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1]=key;
-    }
-
+    mergesort(0,n-1);
     for(int i=0;i<n;i++)
-        cout<<arr[i];
+        cout<<data[i];
     return 0;
+}
+void mergesort(int low, int high)
+{
+    if(high ==low)
+        return;
+    int mid = (low + high)/2;
+
+    if(high > low)
+    {
+        mergesort(low,mid);
+        mergesort(mid+1,high);
+        mergetoget(low,high,mid);
+    }
+}
+void mergetoget(int low, int high, int mid)
+{
+    int i = low;
+    int j = mid+1;
+    for(int k=0;k<=high-low;k++)
+    {
+        if(i==mid+1)
+        {
+            data[k] = arr[j];
+            j++;
+        }
+        else if(j==high+1)
+        {
+            data[k] = arr[i];
+            i++;
+        }
+        else if(arr[i]<arr[j])
+        {
+            data[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            data[k] = arr[j];
+            j++;
+        }
+    }
+    for(int k=0,l=low;l<=high;k++,l++)
+    {
+        arr[l] = data[k];
+    }
 }

4187b2fa01f5464407690d9ae2e8178ee36b48d8
IndolentBear <48911041+indolentbear@users.noreply.github.com>
2019-10-01 23:26:20 +0800

    Update first_2for.cpp

--- MaxPartArray/first_2for.cpp
+++ MaxPartArray/first_2for.cpp
@@ -1,1 +1,27 @@
+/*** 两层循环，遍历 ***/
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n]={0};
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    int maxnum=0;
+    for(int i=0;i<n;i++)
+    {
+        int accadd=arr[i];
+        for(int j=i+1;j<n;j++)
+        {
+            accadd += arr[j];
+            if(accadd>maxnum)
+                maxnum = accadd;
+        }
+    }
+
+    cout<<maxnum;
+    return 0;
+}
